feeb52aa697a0c6b021679d545c6893deba15a81
sutitintaintawong-lang <sutitinta.intawong@gmail.com>
2026-07-22 16:21:59 +0700

    Create status.js

---
+++ js/status.js
@@ -0,0 +1,50 @@
+// ======================================
+// AI Avatar Framework v2.0
+// Status System
+// ======================================
+
+
+// ======================================
+// Set Status
+// ======================================
+
+export function setStatus(text, color) {
+
+
+    const statusText =
+
+        document.getElementById(
+            "statusText"
+        );
+
+
+
+    const statusDot =
+
+        document.getElementById(
+            "statusDot"
+        );
+
+
+
+    if(statusText){
+
+
+        statusText.textContent = text;
+
+
+    }
+
+
+
+    if(statusDot){
+
+
+        statusDot.style.background =
+            color;
+
+
+    }
+
+
+}
