8c871583893856fb95e2151bc7c01fa6f65ceca3
sutitintaintawong-lang <sutitinta.intawong@gmail.com>
2026-07-23 14:44:57 +0700

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -15,6 +15,10 @@ import {
     updateIdle
 } from "./idle.js";
 
+import {
+    updateLipSync
+} from "./lipsync.js";
+
 
 // ======================================
 // Start Animation
@@ -22,94 +26,69 @@ import {
 
 export function startAnimation(app) {
 
-
     function animate() {
 
+        requestAnimationFrame(animate);
 
-        requestAnimationFrame(
-            animate
-        );
-
-
-
-        const delta =
-
-            app.clock.getDelta();
-
-
+        const delta = app.clock.getDelta();
 
         // --------------------------
         // Update VRM
         // --------------------------
 
-        if (app.currentVrm) {
+        if (
+            app.currentVrm &&
+            typeof app.currentVrm.update === "function"
+        ) {
 
-
-            app.currentVrm.update(
-                delta
-            );
-
+            app.currentVrm.update(delta);
 
         }
-
-
 
         // --------------------------
         // Head Tracking
         // --------------------------
 
-        updateLook(
-            app
-        );
-
-
+        updateLook(app);
 
         // --------------------------
         // Blink
         // --------------------------
 
         updateBlink(
-
             app,
-
             delta
-
         );
-
-
 
         // --------------------------
         // Idle Motion
         // --------------------------
 
         updateIdle(
-
             app,
-
             delta
-
         );
 
+        // --------------------------
+        // Lip Sync
+        // --------------------------
 
+        updateLipSync(
+            app,
+            delta
+        );
 
         // --------------------------
         // Render
         // --------------------------
 
         app.renderer.render(
-
             app.scene,
-
             app.camera
-
         );
-
 
     }
 
-
-
     animate();
-
 
 }
