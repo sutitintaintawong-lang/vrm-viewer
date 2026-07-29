fc548730a7d3e53fb87db00d5c52092ce46db264
sutitintaintawong-lang <sutitinta.intawong@gmail.com>
2026-07-29 09:41:53 +0700

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -82,7 +82,7 @@ export function loadVRM(app){
 
                 vrm.scene.rotation.set(
                     0,
-                    0,
+                    Math.PI,
                     0
                 );
 
