45efdf19b47d891c356c064aeeff9cd78de00dfd
sutitintaintawong-lang <sutitinta.intawong@gmail.com>
2026-08-14 14:20:15 +0700

    Adjust camera position and lookAt height

--- js/scene.js
+++ js/scene.js
@@ -47,7 +47,7 @@ export function initScene() {
 
     camera.position.set(
         0,
-        1.10,
+        1.40,
         1.6
     );
 
@@ -59,7 +59,7 @@ export function initScene() {
 
     camera.lookAt(
         0,
-        1.10,
+        1.40,
         0
     );
 
